0681ca5aaf2eb291edf61120c7ceb945e23d2be3
Askbackwards <jjbaddude@hotmail.com>
2015-08-01 19:32:09 -0400

    First Build
    Test build

---
+++ Assets/My Assets/Mechanic/Scripts/Controller.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+
+	public float acceleration = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void GoLeft() {
+		transform.Translate (Vector2.left * Time.deltaTime * acceleration);
+	}
+
+	void GoRight() {
+		transform.Translate (Vector2.right * Time.deltaTime * acceleration);
+	}
+
+	void GoUp() {
+		transform.Translate (Vector2.up * Time.deltaTime * acceleration);
+	}
+}
---
+++ Assets/My Assets/Scripts/PadController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PadController : MonoBehaviour {
+
+	public bool isLeft, isRight, isUp;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount > 0) {
+			for(int i = 0; i <= Input.touchCount; i++) {
+				Touch touch = Input.GetTouch (i);
+				Vector2 touchPos = touch.position;
+				Vector2 thisPos = transform.position;
+				if(touchPos == thisPos) {
+					if (isLeft == true) {
+						SendMessage("GoLeft");
+					}
+					else if (isRight == true) {
+						SendMessage("GoRight");
+					}
+					else if (isUp == true) {
+						SendMessage("GoUp");
+					}
+				}
+			}
+		}
+	}
+}
